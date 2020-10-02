03e367c8c5ac61648434c1ad3b4aec6a3a7b0611
unknown <nadeeshas@insharptechnologies.com>
2020-10-02 16:19:09 +0530

    change socket

--- index.js
+++ index.js
@@ -7,10 +7,11 @@ router.get('/',(req,res)=>{
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     res.setHeader('Access-Control-Allow-Credentials', true);
-
+    console.log("ishi socket running");
     res.send({
         response:"Ishi socket2 server is up and running"
     }).status(200)
+	
 })
 
 
